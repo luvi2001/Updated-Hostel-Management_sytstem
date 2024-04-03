a83a2364a8e7dc82efff8568590273c86b42106c
luvi2001 <luvinson777@gmail.com>
2024-04-04 00:42:16 +0530

    s

--- TechHostel/Backend/server.js
+++ TechHostel/Backend/server.js
@@ -1,13 +1,30 @@
-//Connection file to mongo db
-const express=require('express');
+// Connection file to MongoDB
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+app.use(express.json());
 
-app.get('/',(req,res) => {
-  res.send("Welcome to project Tech?Hostel");
-
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-app.listen(5000,() =>{
-  console.log("SERVWR IS WORKING ON PORT 5000");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.error('Error connecting to database:', err));
+
+app.get('/', (req, res) => {
+  res.send("Welcome to project Tech?H");
+});
+
+app.get('/gf', (req, res) => {
+  res.send("Welcome to project Tech?Hostel");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
