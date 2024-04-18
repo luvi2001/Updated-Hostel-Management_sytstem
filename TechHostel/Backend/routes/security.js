7abf073a8c26a60c2a452c8eb828bcea93d7e636
luvi2001 <luvinson777@gmail.com>
2024-04-18 15:23:05 +0530

    a

--- TechHostel/Backend/routes/security.js
+++ TechHostel/Backend/routes/security.js
@@ -12,7 +12,7 @@ router.post('/apply', applyGatePass);
 // Route for verifying a gate pass
 router.put('/verify/:id', verifyGatePass);
 
-router.post('/delete/:id', deleteGatePassById);
+router.delete('/delete/:id', deleteGatePassById);
 
 router.get('/nic/:nicNumber',getGatePassesByNIC);
 module.exports= router
