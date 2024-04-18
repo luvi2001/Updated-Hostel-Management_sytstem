1fa8ffc2b09abc7c6d4b1305aab3adacc47c8f2a
luvi2001 <luvinson777@gmail.com>
2024-04-18 14:40:55 +0530

    warden

--- TechHostel/Backend/routes/warden.js
+++ TechHostel/Backend/routes/warden.js
@@ -1,12 +1,10 @@
 const express =require('express')
 
-const {registerProfile,getUserByName,approveGatePass}= require('../controllers/wardencontroller')
+const {registerProfile,getUserByName}= require('../controllers/wardencontroller')
 
 const router=express.Router()
 
 router.post('/register',registerProfile)
 router.get('/search',getUserByName)
 
-// Route for approving a gate pass
-router.put('/approve/:id', approveGatePass)
 module.exports= router
