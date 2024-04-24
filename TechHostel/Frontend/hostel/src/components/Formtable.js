c309622ca8abfd88088def3b771db9df37a6d1d8
KarthikRaj1024 <155059907+KarthikRaj1024@users.noreply.github.com>
2024-04-24 10:16:11 +0530

    form

--- TechHostel/Frontend/hostel/src/components/Formtable.js
+++ TechHostel/Frontend/hostel/src/components/Formtable.js
@@ -8,15 +8,15 @@ const Formtable = ({handleSubmit,handleOnChange,handleclose,rest}) => {
     <form onSubmit={handleSubmit}>
     <div className="close-btn" onClick={handleclose}><IoCloseSharp /></div>
       <label htmlFor="name">Name : </label>
-      <input type = "text" id = "name" name="name" onChange={handleOnChange} value={rest.name} required />
+      <input type = "text" id = "name" name="name" onChange={handleOnChange} value={rest.name} readOnly />
 
       <label htmlFor="mobile">NIC : </label>
-      <input type = "tel" id = "mobile" name="nic" onChange={handleOnChange} value={rest.mobile} 
-              required 
+      <input type = "text" id = "mobile" name="nic" onChange={handleOnChange} value={rest.nic} 
+              
               pattern="[0-9]{12}"
               maxLength="12"
               minLength="12"
-              title="Please enter a 10-digit telephone number"/>
+              title="Please enter a 10-digit telephone number" readOnly/>
 
       <label htmlFor="issue">Issue : </label>
       <input type = "text" id = "issue" name="issue" onChange={handleOnChange} value={rest.issue} 
