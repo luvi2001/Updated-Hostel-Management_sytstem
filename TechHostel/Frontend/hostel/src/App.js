11ebd3083a78e0c9b0fbf64e0d187770db3d444a
luvi2001 <luvinson777@gmail.com>
2024-05-13 21:27:44 +0530

    ll

--- TechHostel/Frontend/hostel/src/App.js
+++ TechHostel/Frontend/hostel/src/App.js
@@ -16,6 +16,7 @@ import Gettasks from "./pages/Gettasks";
 import Studentlogin from "./pages/Studentlogin"
 import Studentprof from "./pages/Studentprof";
 import Applygatepass from "./pages/Applygatepass";
+import PaymentVerification from "./pages/PaymentVerification"
 
 
 function App() {
